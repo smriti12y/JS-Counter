9c02bcf3fba41a9e7f6dca9bbfab8f7a5671fdb6
smriti <rajsimmi369@gmail.com>
2024-01-16 14:57:46 +0530

    customizable button is created

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-class Counter {
-    constructor(value = 0) {
-      this.value = value;
-      this.isPaused = false;
-    }
+function totalClick(click) {
+   const totalClicks = document.getElementById('totalClicks');
+   const  sumvalue = parseInt(totalClicks.innerText) +click;
+   console.log(sumvalue + click);
+   totalClicks.innerText=  sumvalue;
+
+   //avoid nagatives 
+
+   if(sumvalue < 0){
+    totalClicks.innerText=  0;
+   }
+
+   //reset 
+   if(click===0){
+    totalClicks.innerText=  0;
+   }
+
+   
 }
 
 
